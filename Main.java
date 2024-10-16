a4828664a7bc025ed486761064d15841137ad50f
Alessio <aless.cal06@gmail.com>
2024-10-16 21:02:01 +0200

    Moved logo image

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import assets.scenes.MainScene;
 import engine.core.*;
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game("Graphics Playground", 1200, 800, "./img/logo.png", 1, false);
+        Game game = new Game("Graphics Playground", 1200, 800, "./logo.png", 1, false);
 
         MainScene m = new MainScene();
         game.addScene(m);
