6571a888b9009a2040557097a818ea766fc69555
roysmanfo <manforoys@gmail.com>
2024-10-18 22:15:36 +0200

    Modify window proportions

--- src/main/java/org/cup/Main.java
+++ src/main/java/org/cup/Main.java
@@ -7,7 +7,12 @@ import org.cup.engine.core.Game;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game("Graphics Playground", 1200, 800, "./logo.png", 1f, false);
+
+        // maintain a 16:9 screen ratio
+        int width = 1400;
+        int height = width / 16 * 9;
+
+        Game game = new Game("Graphics Playground", width, height, "./logo.png", 1f, false);
 
         MainScene m = new MainScene();
         game.addScene(m);
