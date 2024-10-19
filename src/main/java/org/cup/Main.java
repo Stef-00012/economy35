ae841accbec33652723471bb12c0885deb5f4f40
BluwolfNW <130541714+BluwolfNW@users.noreply.github.com>
2024-10-19 21:50:05 +0200

    Resolution changed
    changed the resolution to 1280x720

--- src/main/java/org/cup/Main.java
+++ src/main/java/org/cup/Main.java
@@ -7,7 +7,7 @@ import org.cup.engine.core.Game;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game("Graphics Playground", 1200, 800, "./logo.png", 1f, false);
+        Game game = new Game("Graphics Playground", 1280, 720, "./logo.png", 1f, false);
 
         MainScene m = new MainScene();
         game.addScene(m);
