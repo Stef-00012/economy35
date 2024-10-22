5507d5605422fe4b280d7b58c8e7d805b58bee22
Alessio <aless.cal06@gmail.com>
2024-10-22 15:38:59 +0200

    Set sprite method

--- src/main/java/org/cup/engine/Utils.java
+++ src/main/java/org/cup/engine/Utils.java
@@ -1,5 +1,13 @@
 package org.cup.engine;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.cup.engine.core.Debug;
+
 public class Utils {
     /**
      * Helper function for linear interpolation.
@@ -10,5 +18,15 @@ public class Utils {
      */
     public static double lerp(double a, double b, double t) {
         return a + (b - a) * t;
+    }
+
+    public static Image tryGetImage(String imageSrc){
+        try {
+            return ImageIO.read(new File(imageSrc));
+        } catch (IOException e) {
+            Debug.engineLogErr("Failed to load " + imageSrc);
+            System.exit(-1);
+        }
+        return null;
     }
 }
