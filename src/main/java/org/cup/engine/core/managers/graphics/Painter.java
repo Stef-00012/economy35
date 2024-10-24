8f530ff4b8613a9ebfc872561beb503f421627f4
Alessio <aless.cal06@gmail.com>
2024-10-25 00:03:21 +0200

    Bella questa

--- src/main/java/org/cup/engine/core/managers/graphics/Painter.java
+++ src/main/java/org/cup/engine/core/managers/graphics/Painter.java
@@ -70,7 +70,7 @@ public class Painter extends JPanel {
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         }
 
-        //setDoubleBuffered(true);
+        setDoubleBuffered(true);
 
         for (int i = 0; i < size; i++) {
             Renderer component = queue.get(i);
