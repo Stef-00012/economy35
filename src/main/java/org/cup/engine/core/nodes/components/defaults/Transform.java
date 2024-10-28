a522b4876688508329162c93b766b695bcdfe403
Alessio <aless.cal06@gmail.com>
2024-10-28 20:55:59 +0100

    Optimised transform

--- src/main/java/org/cup/engine/core/nodes/components/defaults/Transform.java
+++ src/main/java/org/cup/engine/core/nodes/components/defaults/Transform.java
@@ -3,8 +3,10 @@ package org.cup.engine.core.nodes.components.defaults;
 import org.cup.engine.Vector;
 
 /**
- * Represents a transformation in a 2D space, including position, scale, and rotation.
- * It supports hierarchical transformations, allowing a child to inherit transformations from its parent.
+ * Represents a transformation in a 2D space, including position, scale, and
+ * rotation.
+ * It supports hierarchical transformations, allowing a child to inherit
+ * transformations from its parent.
  */
 public class Transform {
 
@@ -12,6 +14,12 @@ public class Transform {
     private Vector position = Vector.ONE;
     private Vector scale = Vector.ONE;
     private double rotation = 0; // Rotation in radians
+
+    private boolean isScaleDirty = true;
+    private boolean isPositionDirty = true;
+
+    private Vector cachedPosition = Vector.ONE;
+    private Vector cachedScale = Vector.ONE;
 
     private Transform parentTransform;
 
@@ -22,7 +30,8 @@ public class Transform {
     /**
      * Constructor for Transform with a parent transform.
      *
-     * @param parent The parent Transform from which this Transform inherits position, scale, and rotation.
+     * @param parent The parent Transform from which this Transform inherits
+     *               position, scale, and rotation.
      */
     public Transform(Transform parent) {
         parentTransform = parent;
@@ -35,6 +44,7 @@ public class Transform {
      */
     public void move(Vector v) {
         position = position.add(v);
+        isPositionDirty = true;
     }
 
     /**
@@ -46,18 +56,29 @@ public class Transform {
         rotation += rad;
     }
 
-    //#region Getters & Setters
+    // #region Getters & Setters
 
     /**
-     * Gets the absolute position of this transform, including parent transform if present.
+     * Gets the absolute position of this transform, including parent transform if
+     * present.
      *
      * @return The absolute position as a Vector.
      */
     public Vector getPosition() {
-        if (parentTransform != null) {
-            return parentTransform.getPosition().add(position);
+        if (isPositionDirty || (parentTransform != null && parentTransform.isPositionDirty)) {
+            updatePositionCache();
         }
-        return position;
+        return cachedPosition;
+    }
+
+    private void updatePositionCache() {
+        if (parentTransform != null) {
+            cachedPosition = parentTransform.getPosition().add(position);
+        } else {
+            cachedPosition = position;
+        }
+
+        isPositionDirty = false;
     }
 
     /**
@@ -67,18 +88,30 @@ public class Transform {
      */
     public void setPosition(Vector position) {
         this.position = position;
+        isPositionDirty = true;
     }
 
     /**
-     * Gets the absolute scale of this transform, combining with the parent's scale if present.
+     * Gets the absolute scale of this transform, combining with the parent's scale
+     * if present.
      *
      * @return The absolute scale as a Vector.
      */
     public Vector getScale() {
-        if (parentTransform != null) {
-            return Vector.multiplyVec(scale, parentTransform.getScale()); // Scaling is often multiplicative
+        if (isScaleDirty || (parentTransform != null && parentTransform.isScaleDirty)) {
+            updateScaleCache();
         }
-        return scale;
+        return cachedScale;
+    }
+
+    private void updateScaleCache() {
+        if (parentTransform != null) {
+            cachedScale = Vector.multiplyVec(scale, parentTransform.getScale());
+        } else {
+            cachedScale = scale;
+        }
+
+        isScaleDirty = false;
     }
 
     /**
@@ -88,10 +121,12 @@ public class Transform {
      */
     public void setScale(Vector scale) {
         this.scale = scale;
+        isScaleDirty = true;
     }
 
     /**
-     * Gets the absolute rotation of this transform, including parent rotation if present.
+     * Gets the absolute rotation of this transform, including parent rotation if
+     * present.
      *
      * @return The absolute rotation in radians.
      */
@@ -121,13 +156,33 @@ public class Transform {
     }
 
     /**
+     * The position is dirty if it changed, therefore it needs to update the cache
+     * 
+     * @return true if the position variable has changed
+     */
+    public boolean isPositionDirty() {
+        return isPositionDirty;
+    }
+
+    /**
+     * The scale is dirty if it changed, therefore it needs to update the cache
+     * 
+     * @return true if the scale variable has changed
+     */
+    public boolean isScaleDirty() {
+        return isScaleDirty;
+    }
+
+    /**
      * Sets the parent transform for this transform.
      *
      * @param parentTransform The new parent Transform.
      */
     public void setParentTransform(Transform parentTransform) {
         this.parentTransform = parentTransform;
-    }
-    //#endregion
-}
 
+        isPositionDirty = true;
+        isScaleDirty = true;
+    }
+    // #endregion
+}
