4a56a660891e13b1b53fbe5e1b7f62157c7c4031
roysmanfo <manforoys@gmail.com>
2024-10-28 21:24:24 +0100

    Allow to print objects

--- src/main/java/org/cup/engine/core/Debug.java
+++ src/main/java/org/cup/engine/core/Debug.java
@@ -5,19 +5,19 @@ public class Debug {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_YELLOW = "\u001B[33m";
 
-    public static void log(String text) {
-        System.out.println("[INFO]\t\t" + text);
+    public static void log(Object text) {
+        System.out.println("[INFO]\t\t" + text.toString());
     }
 
-    public static void engineLog(String text) {
-        System.out.println("[ENGINE]\t" + text);
+    public static void engineLog(Object text) {
+        System.out.println("[ENGINE]\t" + text.toString());
     }
 
-    public static void engineLogErr(String text) {
-        System.out.println(ANSI_RED + "[ENGINE]\t" + text + ANSI_RESET);
+    public static void engineLogErr(Object text) {
+        System.out.println(ANSI_RED + "[ENGINE]\t" + text.toString() + ANSI_RESET);
     }
 
-    public static void warn(String text) {
-        System.out.println(ANSI_YELLOW + "[WARN]\t" + text + ANSI_RESET);
+    public static void warn(Object text) {
+        System.out.println(ANSI_YELLOW + "[WARN]\t" + text.toString() + ANSI_RESET);
     }
 }
