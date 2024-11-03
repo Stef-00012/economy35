9c1c6ef783c25ed4e4bdd6f92d576877142cf957
roysmanfo <manforoys@gmail.com>
2024-11-03 13:30:20 +0100

    remove debug info

--- src/main/java/org/cup/assets/UI/GameLabel.java
+++ src/main/java/org/cup/assets/UI/GameLabel.java
@@ -3,7 +3,6 @@ package org.cup.assets.UI;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.io.File;
-import java.io.IOException;
 
 import javax.swing.JLabel;
 
@@ -11,16 +10,14 @@ import org.cup.assets.PathHelper;
 import org.cup.engine.core.Debug;
 
 public class GameLabel extends JLabel {
-    public GameLabel(String text) {
+    public GameLabel(String text) { 
         super(text);
 
         try {
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             Font f = Font.createFont(Font.TRUETYPE_FONT, new File(PathHelper.fonts + "Font.ttf"));
             ge.registerFont(f);
-            setFont(new Font(f.getFontName(), Font.PLAIN, 30));
-            
-            System.out.println(getFont());
+            setFont(new Font(f.getFontName(), Font.PLAIN, 30));            
         } catch (Exception e) {
             Debug.engineLogErr(e.getMessage());
         }
