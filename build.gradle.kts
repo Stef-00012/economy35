c766bb6670aaa1be4ef1a1203a4f3a8117e665b5
roysmanfo <manforoys@gmail.com>
2024-11-04 20:49:47 +0100

    rollback as the new settings don't work for some reason

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,25 +17,3 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
-
-// Define specific asset directories as resources
-sourceSets {
-    main {
-        resources {
-            srcDirs("src/main/java/org/cup/assets/sprites", "src/main/java/org/cup/assets/UI")
-        }
-    }
-}
-
-// Configure the Jar task to include only specified assets directories
-tasks.withType<Jar> {
-    manifest {
-        attributes["Main-Class"] = "org.cup.Main"
-    }
-    from("src/main/java/org/cup/assets/sprites") {
-        into("org/cup/assets/sprites")
-    }
-    from("src/main/java/org/cup/assets/UI") {
-        into("org/cup/assets/UI")
-    }
-}
