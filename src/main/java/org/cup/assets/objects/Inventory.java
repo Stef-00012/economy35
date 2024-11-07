51e06b22a23e8617352661849ee9699532cfd14a
Alessio <96922088+AlessTheDev@users.noreply.github.com>
2024-11-07 18:12:54 +0100

    Inventory buttons

--- src/main/java/org/cup/assets/objects/Inventory.java
+++ src/main/java/org/cup/assets/objects/Inventory.java
@@ -1,15 +1,19 @@
 package org.cup.assets.objects;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 import org.cup.assets.PathHelper;
 import org.cup.assets.UI.Floor;
+import org.cup.assets.UI.GameButton;
 import org.cup.assets.scenes.MainScene;
 import org.cup.engine.Vector;
 import org.cup.engine.core.Debug;
 import org.cup.engine.core.nodes.components.Renderer;
 import org.cup.engine.core.nodes.components.defaults.SpriteRenderer;
-import org.cup.engine.core.nodes.components.defaults.SquareRenderer;
+import org.cup.assets.logic.Economy;
 
 /* 
  * Graphical part of the inventory
@@ -20,10 +24,22 @@ public class Inventory extends Floor {
     private int buffer;
     private int maxSize;
 
+    private int newFloorCost = 500;
+    private int increaseInventoryCost = 80;
+
+    // UI
+    private JPanel buttonsPanel = new JPanel();
+    GameButton newFloorBtn = new GameButton(
+            "<html><center>" + "ADD NEW FLOOR" + "<br>($" + newFloorCost + ")</center></html>");
+    GameButton increaseInventoryBtn = new GameButton(
+            "<html><center>" + "INCREASE INVENTORY" + "<br>($" + increaseInventoryCost + ")</center></html>");
+
     public Inventory(int initialMaxSize) {
         buffer = 0;
         maxSize = initialMaxSize;
         updateInventoryStatsUI();
+
+        initUI();
     }
 
     public void init() {
@@ -78,8 +94,30 @@ public class Inventory extends Floor {
         MainScene.getStatsPanel().setInventoryLabel(this);
     }
 
+    private void initUI() {
+        buttonsPanel.setLayout(new GridLayout(1, 2, 10, 0));
+
+        increaseInventoryBtn.setColors(
+                new Color(50, 192, 37),
+                new Color(37, 160, 26));
+
+        newFloorBtn.addActionListener(e -> {
+            Economy.spendMoney(newFloorCost);
+            Building.get().addRoom();
+        });
+
+        increaseInventoryBtn.addActionListener(e -> {
+            Economy.spendMoney(increaseInventoryCost);
+            maxSize++;
+            MainScene.getStatsPanel().setInventoryLabel(this);
+        });
+
+        buttonsPanel.add(newFloorBtn);
+        buttonsPanel.add(increaseInventoryBtn);
+    }
+
     @Override
     public JPanel getUI() {
-        return new JPanel();
+        return buttonsPanel;
     }
 }
